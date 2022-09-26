cf3c2eeecb1fd5266c9da274522d89ec05694fc8
RishatGR <ural.surgut.galiev@gmail.com>
2022-09-26 12:19:41 +0500

    newproject

---
+++ main.py
@@ -0,0 +1,9 @@
+# python3 -m venv venv
+# source venv/bin/activate
+# pip3 install django
+# django-admin startproject NewsPaper
+# cd NewsPaper
+# python3 manage.py startapp news
+# python3 manage.py startapp accounts
+# python3 manage.py runserver - запуск проекта
+# python3 manage.py migrate - миграцмя установленных приложений

eb8495c5c2655ff91ab8028f37f4a80d9ccb6c59
RishatGR <ural.surgut.galiev@gmail.com>
2022-10-15 13:37:47 +0500

    Итоговая версия

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,45 +1,51 @@
 from django.db import models
-
-director = 'DI'
-admin = 'AD'
-cook = 'CO'
-cashier = 'CA'
-cleaner = 'CL'
-
-POSITIONS = [
-    (director, 'Директор'),
-    (admin, 'Администратор'),
-    (cook, 'Повар'),
-    (cashier, 'Кассир'),
-    (cleaner, 'Уборщик')
-]
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
-class Staff(models.Model):
-    full_name = models.CharField(max_length=255)
-    position = models.CharField(max_length=255)
-    labor_contract = models.IntegerField()
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField(default=0.0)
-    composition = models.TextField(default="Состав не указан")
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = RichTextUploadingField()
+    image = models.ImageField()
+    created_at = models.DateField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
 
 
-class Order(models.Model):
-    time_in = models.DateTimeField(auto_now_add=True)
-    time_out = models.DateTimeField(null=True)
-    cost = models.FloatField(default=0.0)
-    pickup = models.BooleanField(default=False)
-    complete = models.BooleanField(default=False)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
-    products = models.ManyToManyField(Product, through='ProductOrder')
+    def get_absolute_url(self):
+        return f'/{self.id}'
 
 
-class ProductOrder(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=255,  verbose_name='Текст')
+
+    comment_data = models.DateField(auto_now_add=True)
+    accepted = models.BooleanField(default=False, verbose_name='Принято')
+
+    #class Meta:
+        #unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f'{self.text}'
+
+    def is_accept(self):
+        return self.accepted
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# python3 -m venv venv
+# python -m venv venv
 # source venv/bin/activate
 # pip3 install django
 # django-admin startproject NewsPaper
 # cd NewsPaper
-# python3 manage.py startapp news
-# python3 manage.py startapp accounts
-# python3 manage.py runserver - запуск проекта
-# python3 manage.py migrate - миграцмя установленных приложений
-# python3 manage.py makemigrations - создание миграций
+# python manage.py startapp news
+# python manage.py startapp accounts
+# python manage.py runserver - запуск проекта
+# python manage.py migrate - миграцмя установленных приложений
+# python manage.py makemigrations - создание миграций
